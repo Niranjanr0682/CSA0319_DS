0819b36947d63dc9810f14342bbbc8a8636be63b
Niranjan Rajkumar <niranjanr0682.sse@saveetha.com>
2023-05-03 14:31:28 +0530

    update

--- Class work/6.c
+++ Class work/6.c
@@ -27,12 +27,14 @@
 
 int main() 
     {
-    int a[100][100], b[100][100], d[100][100],i,j,k,r,c;                               // for a matrix
+    int a[100][100], b[100][100], d[100][100],i,j,k,r,c,x,y,z;                               // for a matrix
     printf("enter how many rows and columns for a\n");                                 // for a matrix
     printf("enter rows   :" );                                                         // for a matrix
     scanf("%d",&r);                                                                    // for a matrix
     printf("enter column :" );                                                         // for a matrix
     scanf("%d",&c);                                                                    // for a matrix
+    x = r;
+    y = c;
     printf("Enter elements of 1st matrix\n");                                          // for a matrix
     for (i = 0; i < r; ++i)// for row                                                  // for a matrix
         {for (j = 0; j < c; ++j)// for column                                          // for a matrix
@@ -43,17 +45,25 @@ int main()
     scanf("%d",&r);                                                                    // for b matrix
     printf("enter column :" );                                                         // for b matrix
     scanf("%d",&c);                                                                    // for b matrix
+    z = r;
     printf("Enter elements of 2nd matrix\n");                                          // for b matrix
     for (i = 0; i < r; ++i)                                                            // for b matrix
         {for (j = 0; j < c; ++j)                                                       // for b matrix
             {printf("Enter element b[%d][%d]: ", i + 1, j + 1 );                       // for b matrix
             scanf("%d", &b[i][j]);}}                                                   // for b matrix
     // multiplying two matrices   
-    for(i=0;i<r;i++)    
-        {for(j=0;j<c;j++)    
-            {d[i][j]=0;    
-                for(k=0;k<c;k++)    
-                    {d[i][j]+=a[i][k]*b[k][j];}}}  
+    // for(i=0;i<r;i++)    
+    //     {for(j=0;j<c;j++)    
+    //         {d[i][j]=0;    
+    //             for(k=0;k<c;k++)    
+    //                 {d[i][j]+=a[i][k]*b[k][j];}}}  
+    for(i=0;i<x;i++)    
+        {for(k=0;k<z;k++)    
+            {d[i][k]=0;    
+                for(j = 0;j<y;j++)    
+                    {d[i][k]+=(a[i][j]*b[j][k]);}}}  
+    
+
     // printing the result
     printf("\nmultiply of two matrices: \n");
     for (i = 0; i < r; ++i)
